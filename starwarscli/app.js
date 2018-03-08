59063dd239a4b8f4f84d79db9bc6988fedd21183
briank <brian.a.kan@gmail.com>
2018-03-07 20:17:01 -0800

    Gives a checkbox list for when multiple results are returned

--- starwarscli/app.js
+++ starwarscli/app.js
@@ -2,34 +2,53 @@ const
     starwars = require('starwarsapi'),
     inquirer = require('inquirer')
 
-const search = (category, query) => {
+const search = (category, query) => { // 
+    const nameArray = []
     starwars.search(category, query)
         .then((results) => {
-           results.results.forEach((item)=>{
-               console.log(item.name)
-           })
+            results.results.forEach((item) => {
+                nameArray.push(item.name)
+            })
+            results["category"]=category;
+            if (nameArray.length > 1) {
+                promptUser(results, nameArray)
+            }
+            else {
+                console.log(results)
+            }
         }).catch(err => console.log(err))
 }
 
 const getbyid = (category, id) => {
     starwars.getbyid(category, id)
         .then((results) => {
-           console.log(results)
+            console.log(results)
         }).catch(err => console.log(err))
 }
 
 
-
-const discardPrompt = (result, currentHand, removeCrds) => {
+const promptUser = (results, nameArray) => {
     return inquirer.prompt([{
         type: 'checkbox',
         message: 'Select to view details',
-        name: 'cards',
-        choices: currentHand,
-        validate: (cards) => { }
-    }])
+        name: 'selections',
+        choices: nameArray,
+        validate: (selections) => {
+            if (selections.length > 0) {
+                return true
+            }
+        }
+    }]).then((selected) => {
+       results.results =results.results.filter((item)=>{
+           return selected.selections.includes(item.name)
+       })
+       console.log(results)
+    })
 }
 
+const prettyPrint = (object) => {
+
+}
 
 module.exports = {
     search,
