613ba2c83d4ee60cf7b49c2ae22a3c50bc53f4e2
briank <brian.a.kan@gmail.com>
2018-03-18 19:09:35 -0700

    Fixed up some printing

--- starwarscli/app.js
+++ starwarscli/app.js
@@ -50,43 +50,117 @@ const promptUser = (results, nameArray) => {
 const prettyPrint = (object) => {
     //[films|people|planets|species|starships|vehicles]
     // for (results in results) print each object
+    console.log(`
+    ------------------------------  ${object["category"].toUpperCase()}  -------------------------------------`)
     switch (object["category"]) {  // TODO Formatting
-        // case "films":
-
-        //     break;
-        // case "people":
-
-        //     break;
-        // case "planets":
-
-        //     break;
-        // case "species":
-
-        //     break;
-        // case "starships":
-
-        //     break;
-        case "vehicles":
-        console.log("-----------------------------------------------------------------")
+        case "films":  // FILMS -------------------------------------------
             object.results.forEach((item) => {
                 console.log(
-                `
-                >>> ${item.name} <<<
-                Model: ${item.model}
-                Class: ${item.vehicle_class}
-                Manufacturer: ${item.manufacturer}
-                Length: ${item.length} feet
-                Cost: ${item.cost_in_credits} credits
-                Length: ${item.max_atmosphering_speed} 360 km/h 
-                Passengers: ${item.crew}
+                    `
+            >>> ${item.title} <<< 
+                Episode: ${item.episode_id}
+                Director: ${item.director}
+                Producer: ${item.producer}
+                Release Date: ${item.release_date}
+                URL: ${item.URL}
+                Created: ${item.created}
+                Edited: ${item.edited}
+            `
+                )
+            })
+            // Opening Crawl: ${item.opening_crawl}
+            // Species: ${item.species}
+            // Characters: ${item.characters}
+            // Planets: ${item.planets}
+            break;
+        case "people": // PEOPLE --------------------------------
+            object.results.forEach((item) => {
+                console.log(
+                    ` 
+            >>> ${item.name} <<<
+                Birth Year: ${item.birth_year}
+                Eye Color: ${item.eye_color}
+                Hair Color: ${item.hair_color}
+                Gender: ${item.gender}
+                Height: ${item.height}
+                Weight: ${item.mass}
+                Skin Color: ${item.skin_color}
+                Homeworld: ${item.homeworld}
+                Vehicles: ${item.vehicles}
+                URL: ${item.url}
+                Created: ${item.created}
+                Edited: ${item.edited}
                 `
                 )
             })
-            console.log("-----------------------------------------------------------------")
+            // Films: ${item.films}
+            // Species: ${item.species}                           
+            // Starships: ${item.starships}
+            break;
+        // case "planets":
+        //TODO
+        //     break;
+        case "species":   //SPECIES -----------------------------------
+            object.results.forEach((item) => {
+                console.log(
+                    `
+            >>> ${item.name} <<<
+                Average Height: ${item.average_height}
+                Average Lifespan: ${item.average_lifespan}
+                Classification: ${item.classification} 
+                Designation: ${item.designation}       
+                Eye Colors: ${item.eye_colors}
+                Hair Colors: ${item.hair_colors}
+                Skin Colors: ${item.skin_colors}  
+                Homeworld: ${item.homeworld}
+                Language: ${item.language}                        
+                URL: ${item.url}
+                Created: ${item.created}
+                Edited: ${item.edited}
+             `
+                )
+            })
+            // People: ${item.people}  
+            // Films: ${item.films}    
+            break;
+        case "starships":  // STARSHIPS     ----------------------
+            object.results.forEach((item) => {
+                console.log(
+                    `
+            >>> ${item.name} <<<
+                Model: ${item.model}
+                Class: ${item.starship_class}
+                Manufacturer: ${item.manufacturer}
+                Length: ${item.length} feet
+                Cost: ${item.cost_in_credits} credits 
+                Passengers: ${item.passengers}
+                Crew: ${item.crew}
+                Cargo: ${item.cargo_capacity}
+                Max atmospheric Pressure: ${item.max_atmosphering_speed} km/h
+                `
+                )
+            })
+            break;
+        case "vehicles":
+            object.results.forEach((item) => {
+                console.log(
+                    `
+            >>> ${item.name} <<<
+               Model: ${item.model}
+               Class: ${item.vehicle_class}
+               Manufacturer: ${item.manufacturer}
+               Length: ${item.length} feet
+               Cost: ${item.cost_in_credits} credits
+               Length: ${item.max_atmosphering_speed} km/h 
+               Passengers: ${item.crew}
+                `
+                )
+            })
             break;
         default:
             console.log(object);
             break;
+            console.log("-----------------------------------------------------------------")
     }
 
 }
